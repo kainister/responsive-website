e9e03be32d2ca19f2101dd5d70c9d45c6993cf6d
Kevin Badinca <kevin.badinca@supinternet.fr>
2018-03-25 20:34:59 +0200

    added contact

--- assets/script.js
+++ assets/script.js
@@ -7,4 +7,10 @@ window.onload = function () {
         document.getElementById("mySidenav").style.width = "250px";
 
     };
+    var sideNav = document.querySelectorAll('.side-nav-a');
+    for (var i = 0; i < sideNav.length; i++) {
+        sideNav[i].onclick = function () {
+            document.getElementById("mySidenav").style.width = "0";
+        }
+    }
 };
